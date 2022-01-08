aded6adef4a7c3547d8f4844b3cd5658983c97fa
Damian Tobiczyk <damian.tobiczyk@gmail.com>
2022-01-08 20:19:22 +0100

    add CORS mapping

--- aquapark-backend/src/main/java/pl/polsl/telinf/s3/configuration/SecurityConfig.java
+++ aquapark-backend/src/main/java/pl/polsl/telinf/s3/configuration/SecurityConfig.java
@@ -1,7 +1,6 @@
 package pl.polsl.telinf.s3.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +11,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.polsl.telinf.s3.repository.jpa.JPAUserRepository;
 import pl.polsl.telinf.s3.security.JwtTokenFilter;
 import pl.polsl.telinf.s3.security.Role;
@@ -21,7 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 import static java.lang.String.format;
 
 @EnableWebSecurity
-class SecurityConfig extends WebSecurityConfigurerAdapter {
+class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     private JPAUserRepository userRepository;
     private final JwtTokenFilter jwtTokenFilter;
@@ -82,6 +83,11 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
         );
 
 
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
     @Override @Bean
