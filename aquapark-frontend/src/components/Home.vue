55cb233bf19897152d57ef1f6d209dd1fd1715f5
gg <sproket176@gmail.com>
2022-01-17 22:35:20 +0100

    Dynamic price list fetch (from API)

--- aquapark-frontend/src/components/Home.vue
+++ aquapark-frontend/src/components/Home.vue
@@ -28,30 +28,18 @@
           </div>
         </div>
       </div>
+
       <div class="priceList" id="priceList">
         <h2 class="title">Cennik</h2>
-        <div class="priceBox">
+        <div class="priceBox" v-for="priceItem in priceItems" :key="priceItem.id">
           <div class="row">
             <div class="col-sm-6">
-              <h2>Ulgowy (rodzinny)</h2>
-              <p>Cena za osobę, ulgowy dla dzieci poniżej lat 13</p>
+              <h2>{{ priceItem.ticket.ticketType.type === 'normal' ? 'Normalny' : 'Ulgowy' }}</h2>
             </div>
             <div class="col-sm-6" style="text-align: right;">
-              <p class="price">25 zł / h</p>
-              <a data-bs-toggle="modal" data-bs-target="#priceModal" class="btn btn-success btn-buyTicket"
-                 @click="reduced = 1">Kup bilet</a>
-            </div>
-          </div>
-        </div>
-        <div class="priceBox">
-          <div class="row">
-            <div class="col-sm-6">
-              <h2>Normalny</h2>
-            </div>
-            <div class="col-sm-6" style="text-align: right;">
-              <p class="price">30 zł / h</p>
-              <a data-bs-toggle="modal" data-bs-target="#priceModal" class="btn btn-success btn-buyTicket"
-                 @click="reduced = 0">Kup bilet</a>
+              <p class="price">{{ priceItem.price }} zł / h</p>
+              <a data-bs-toggle="modal" data-bs-target="#priceModal" @click="sendPrice(priceItem.price)"
+                 class="btn btn-success btn-buyTicket">Kup bilet</a>
             </div>
           </div>
         </div>
@@ -86,9 +74,8 @@
                 <input type="text" :value="quantity" readonly>
                 <button @click="increment()">&#xff0b;</button>
               </div>
-              <p class="finalPrice" v-if="reduced">{{ price1 * quantity }} zł</p>
-              <p class="finalPrice" v-if="!reduced">{{ price2 * quantity }} zł</p>
-              <a href="kup" class="btn btn-success btn-buyTicket">Kup bilet</a>
+              <p class="finalPrice">{{ quantity * price }} zł</p>
+              <a class="btn btn-success btn-buyTicket">Kup bilet</a>
             </div>
           </div>
         </div>
@@ -98,19 +85,23 @@
 </template>
 
 <script>
-import Navbar from "./Navbar";
+import Navbar from './Navbar'
+import axios from 'axios'
+
 export default {
   name: 'Home',
   components: {Navbar},
   data() {
     return {
-      reduced: false,
       quantity: 1,
-      price1: 25,
-      price2: 30
+      priceItems: [],
+      price: null
     }
   },
   methods: {
+    sendPrice(price) {
+      this.price = price
+    },
     increment() {
       this.quantity++
     },
@@ -120,7 +111,26 @@ export default {
       } else {
         this.quantity--
       }
-    }
+    },
+    getPriceItems() {
+      let self = this
+      axios.get('http://localhost:8081/api/priceList/actualPriceItems')
+          .then((res) => {
+            self.priceItems = res.data
+          })
+          .catch((err) => {
+            if (err.response.status === 400) {
+              self.$toastr.e("Niepoprawnie wypełnione lub puste pola.")
+            } else if (err.response.status === 401) {
+              self.$toastr.e("Błąd autoryzacji.")
+            } else {
+              self.$toastr.e("Wystąpił błąd wewnętrzny.")
+            }
+          })
+    },
+  },
+  mounted() {
+    this.getPriceItems()
   }
 }
 </script>
